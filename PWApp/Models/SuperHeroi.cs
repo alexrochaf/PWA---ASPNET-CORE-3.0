4025406445998d4146ece11726e2533d8d13a473
Andre Honorio <andrehas7@gmail.com>
2019-11-11 19:35:07 -0400

    Classe de super-heroi

---
+++ PWApp/Models/SuperHeroi.cs
@@ -0,0 +1,22 @@
+﻿namespace PWApp.Models
+{
+    public class SuperHeroi
+    {
+        protected SuperHeroi()
+        {
+        }
+
+        public SuperHeroi(int? id, string nome, string superPoder, byte[] foto)
+        {
+            Id = id ?? 0;
+            Nome = nome;
+            SuperPoder = superPoder;
+            Foto = foto;
+        }
+
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string SuperPoder { get; set; }
+        public byte[] Foto { get; set; }
+    }
+}
