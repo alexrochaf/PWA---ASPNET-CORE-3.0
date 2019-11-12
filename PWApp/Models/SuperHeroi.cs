9dd9b047c67d19517e32cd79f51db2e9bdafcb9f
Andre Honorio <andrehas7@gmail.com>
2019-11-11 20:03:51 -0400

    properties criadas e com data anottations

--- PWApp/Models/SuperHeroi.cs
+++ PWApp/Models/SuperHeroi.cs
@@ -1,4 +1,7 @@
-﻿namespace PWApp.Models
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace PWApp.Models
 {
     public class SuperHeroi
     {
@@ -6,7 +9,7 @@
         {
         }
 
-        public SuperHeroi(int? id, string nome, string superPoder, byte[] foto)
+        public SuperHeroi(int? id, string nome, string superPoder, IFormFile foto)
         {
             Id = id ?? 0;
             Nome = nome;
@@ -14,9 +17,26 @@
             Foto = foto;
         }
 
+        [ScaffoldColumn(false)]
         public int Id { get; set; }
+
+        [Display(Name = "Nome"), Required(ErrorMessage = "Campo {0} é obrigatório")]
         public string Nome { get; set; }
+
+        [Display(Name = "Superpoder"), Required(ErrorMessage = "Campo {0} é obrigatório")]
         public string SuperPoder { get; set; }
-        public byte[] Foto { get; set; }
+
+        [Display(Name = "Foto")]
+        public IFormFile Foto { get; set; }
+
+        [ScaffoldColumn(false)]
+        public string FileName { get; set; }
+
+        [ScaffoldColumn(false)]
+        public string ContentType { get; set; }
+
+        [ScaffoldColumn(false)]
+        public byte[] Imagem { get; set; }
+
     }
 }
