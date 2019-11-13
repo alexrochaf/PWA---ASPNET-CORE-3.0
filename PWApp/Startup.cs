659cca8edea3a24d35758c94f72c8276b9d5f391
Alex 'KaV' Rocha <alexrocha.f@outlook.com>
2019-11-13 16:59:34 -0400

    Update Startup.cs

--- PWApp/Startup.cs
+++ PWApp/Startup.cs
@@ -54,7 +54,7 @@ namespace PWApp
             app.UseStaticFiles(new StaticFileOptions
             {
                 FileProvider = new PhysicalFileProvider(
-                    Path.Combine("D:\\home\\site", "Uploads")),
+                    Path.Combine("D:/home/site", "Uploads")),
                 RequestPath = "/Uploads"
             });
 
